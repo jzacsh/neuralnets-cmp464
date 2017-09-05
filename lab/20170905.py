e05c09fab36aa6564e6a635e3fd7fccbb355a18c
Jonathan Zacsh <jzacsh@gmail.com>
2017-09-05 12:42:48 -0400

    refactor to share state, easier to read

--- lab/20170905.py
+++ lab/20170905.py
@@ -12,24 +12,52 @@ import matplotlib.pyplot as plt
 weight=0.5
 bias=0.2
 
-def buildTrainingSet():
-    setsize=2
-    xvals = 10*np.random.randn(setsize)
-    # see: https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.shape.html
-    #print("x scalars, of shape: %s\n%s" % (xvals.shape, xvals))
+class TrainingSet:
+    def __init__(self, x, y):
+        self.xvals = x
+        self.yvals = y
 
-    # yvals subset of {1, -1}
-    yvals = 2*np.random.randint(size=setsize, low=0, high=2)-1
-    #print("y scalars, of shape: %s\n%s" % (yvals.shape, yvals))
-    return (xvals, yvals)
+    def costhandler(self, weightAndBias):
+        """Handler for numpy's minimize() function"""
+        return self.costof(weightAndBias[0], weightAndBias[1])
 
-def cost(xs, ys):
-    return np.square(weight*xs + bias - ys).sum()
+    def costof(self, weight, bias):
+        """calculates cost using default methodology"""
+        return self.costViaSquare(weight, bias)
+
+    def costViaSquare(self, weight, bias):
+        return np.square(weight*self.xvals + bias - self.yvals).sum()
+
+    def randGuessMimizes(self):
+        for i in range(0, 5):
+            initialGuess = np.random.randn(2)
+            res = minimize(self.costhandler, initialGuess, method='Nelder-Mead')
+            print("\tminimized: %s\t[init guess #%d: %s]" %(res.x, i, initialGuess))
+
+        print("context:\n\tx: %s\n\ty: %s" %(self.xvals, self.yvals))
+        return res # whatever the last mimizer returned
+
+    def buildRandomTrainer():
+        setsize=2
+        xvals = 10*np.random.randn(setsize)
+        # see: https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.shape.html
+        #print("x scalars, of shape: %s\n%s" % (xvals.shape, xvals))
+
+        # yvals subset of {1, -1}
+        yvals = 2*np.random.randint(size=setsize, low=0, high=2)-1
+        #print("y scalars, of shape: %s\n%s" % (yvals.shape, yvals))
+        return TrainingSet(xvals, yvals)
 
 def main():
-    x, y = buildTrainingSet()
-    print("x: %s\ny: %s\n" %(x, y))
-    print("cost:\t%s\n" %(cost(x, y)))
+    print("HARDCODED training set:")
+    ts = TrainingSet(np.array([0, 1]), np.array([1, -1]))
+    print("mimized to:\t%s" % (ts.randGuessMimizes().x))
+    print("cost at above:\t%s\n" %(ts.costof(-2, 1)))
+
+    print("RANDOM training set:")
+    minimal = TrainingSet.buildRandomTrainer().randGuessMimizes()
+    print("mimized to:\t%s" % (minimal.x))
+    print("cost at above:\t%s\n" %(ts.costof(minimal.x[0], minimal.x[1])))
 
 if __name__ == '__main__':
     main()
