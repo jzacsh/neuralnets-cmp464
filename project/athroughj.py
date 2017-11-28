4c4d349e408589431810cdb2d45af518e188405b
Jonathan Zacsh <jzacsh@gmail.com>
2017-11-28 11:06:43 -0500

    noop: IPython not actually used in this file

--- project/athroughj.py
+++ project/athroughj.py
@@ -7,7 +7,10 @@ import numpy as np
 import os
 import sys
 import tarfile
-from IPython.display import display, Image
+
+#was commented out below in Schneider's copy
+# from IPython.display import display, Image
+
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from urllib.request import urlretrieve
