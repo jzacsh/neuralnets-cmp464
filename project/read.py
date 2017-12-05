41adba94deef55471997ed6cece491fbcd4828ad
Jonathan Zacsh <jzacsh@gmail.com>
2017-12-05 12:05:42 -0500

    noop(refactor) encapsulate data being passed around
    make it easier to distinguish core data and its labels from upcoming
    slew of new varaiables I'll have with tensorflow

--- project/read.py
+++ project/read.py
@@ -5,51 +5,65 @@ import sys
 import pickle
 import numpy as np
 
-print("loading pickle file:\n\t%s\n" % sys.argv[1])
+# global settings #############################################################
+batch_size = 128 # the N for the minibatches
+###############################################################################
 
-pklpath = sys.argv[1]
-with open(pklpath, 'rb') as f:
-    save = pickle.load(f)
-    train_dataset = save['train_dataset']
-    train_labels = save['train_labels']
-    valid_dataset = save['valid_dataset']
-    valid_labels = save['valid_labels']
-    test_dataset = save['test_dataset']
-    test_labels = save['test_labels']
-    del save  # hint to help gc free up memory
-    f.close()
-
-def statsStr(data, labels):
-    return "%d labels, %d data points in a %s" % (len(labels), len(data), data.shape)
-
-print("done loading pickle file;\n\t[taining] %s\n\t[testing] %s\n\t[valids ] %s\n" % (
-    statsStr(train_dataset, train_labels),
-    statsStr(test_dataset, test_labels),
-    statsStr(valid_dataset, valid_labels)))
-
-for i in range(15,21):
-    print(train_labels[i])
-
+# TODO: fix to at least assert these are sane values
 image_size = 28
 num_labels = 10
 oneHotDim = (-1, image_size * image_size) # "-1": don't touch first dimension
 
-def reformat(dataset, labels):
-    # takes the Array to 2 dimensional array NoDataXNofeatures
-    dataset = dataset.reshape(oneHotDim).astype(np.float32)
+class DataLabel:
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
 
-    # to make ONE HOT ENCODING; the None adds a dimension and tricky numpy broadcasting
-    labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
-    return dataset, labels
+    def toString(self):
+        return "%d labels, %d data points in a %s" % (
+                len(self.labels), len(self.data), self.data.shape)
 
-train_dataset, train_labels = reformat(train_dataset, train_labels)
-valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
-test_dataset, test_labels = reformat(test_dataset, test_labels)
+    def toHotEncoding(self):
+        # takes the Array to 2 dimensional array NoDataXNofeatures
+        self.data = self.data.reshape(oneHotDim).astype(np.float32)
 
-print("REFORMATED data to ONE HOT encoding;\n\t[taining] %s\n\t[testing] %s\n\t[valids ] %s\n" % (
-    statsStr(train_dataset, train_labels),
-    statsStr(test_dataset, test_labels),
-    statsStr(valid_dataset, valid_labels)))
+        # to make ONE HOT ENCODING; the None adds a dimension and tricky numpy broadcasting
+        self.labels = (np.arange(num_labels) == self.labels[:,None]).astype(np.float32)
 
+class Datas:
+    def __init__(self, training, valid, testing):
+        self.training = training
+        self.valid = valid
+        self.testing = testing
+
+    def string(self):
+        return "\t[training] %s\n\t[ testing] %s\n\t[ valids ] %s" % (
+            self.training.toString(), self.testing.toString(),
+            self.valid.toString())
+
+    @staticmethod
+    def fromPicklePath(pklpath):
+        print("loading pickle file: %s\n" % pklpath)
+        with open(pklpath, 'rb') as f:
+            pkdt = pickle.load(f)
+            d = Datas(
+                    DataLabel(pkdt['train_dataset'], pkdt['train_labels']),
+                    DataLabel(pkdt['valid_dataset'], pkdt['valid_labels']),
+                    DataLabel(pkdt['test_dataset'], pkdt['test_labels']))
+            del pkdt  # hint to help gc free up memory
+            f.close()
+        print("done loading pickle file;\n%s\n" % (d.string()))
+        return d
+
+    def toHotEncoding(self):
+        self.training.toHotEncoding()
+        self.valid.toHotEncoding()
+        self.testing.toHotEncoding()
+
+dataSets = Datas.fromPicklePath(sys.argv[1])
 for i in range(15,21):
-    print(train_labels[i,:])
+    print(dataSets.training.labels[i])
+dataSets.toHotEncoding()
+print("REFORMATED data to ONE HOT encoding;\n%s\n" % dataSets)
+for i in range(15,21):
+    print(dataSets.training.labels[i,:])
