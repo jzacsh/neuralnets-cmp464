227df97cf94a5221369166dba31067940e1a852e
Jonathan Zacsh <jzacsh@gmail.com>
2017-12-05 10:42:22 -0500

    one-hot encoding; following along http://comet.lehman.cuny.edu/schneider/Fall17/CMP464/DemoPrograms/UdacityLetterRecWithMinibatch.py

--- project/read.py
+++ project/read.py
@@ -3,7 +3,7 @@ demonstrating we can load and intepret pickle files
 """
 import sys
 import pickle
-import numpy
+import numpy as np
 
 print("loading pickle file:\n\t%s\n" % sys.argv[1])
 
@@ -26,3 +26,29 @@ print("done loading pickle file;\n\t[taining] %s\n\t[testing] %s\n\t[valids ] %s
     statsStr(test_dataset, test_labels),
     statsStr(valid_dataset, valid_labels)))
 
+for i in range(15,21):
+    print(train_labels[i])
+
+image_size = 28
+num_labels = 10
+oneHotDim = (-1, image_size * image_size) # "-1": don't touch first dimension
+
+def reformat(dataset, labels):
+    # takes the Array to 2 dimensional array NoDataXNofeatures
+    dataset = dataset.reshape(oneHotDim).astype(np.float32)
+
+    # to make ONE HOT ENCODING; the None adds a dimension and tricky numpy broadcasting
+    labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+    return dataset, labels
+
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+
+print("REFORMATED data to ONE HOT encoding;\n\t[taining] %s\n\t[testing] %s\n\t[valids ] %s\n" % (
+    statsStr(train_dataset, train_labels),
+    statsStr(test_dataset, test_labels),
+    statsStr(valid_dataset, valid_labels)))
+
+for i in range(15,21):
+    print(train_labels[i,:])
