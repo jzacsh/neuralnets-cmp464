3bc9a3d2e6465ee38ebcbc871ad9220e713fe381
Jonathan Zacsh <jzacsh@gmail.com>
2017-12-05 09:39:16 -0500

    get started interpreting pickle files

---
+++ project/read.py
@@ -0,0 +1,28 @@
+"""
+demonstrating we can load and intepret pickle files
+"""
+import sys
+import pickle
+import numpy
+
+print("loading pickle file:\n\t%s\n" % sys.argv[1])
+
+pklpath = sys.argv[1]
+with open(pklpath, 'rb') as f:
+    save = pickle.load(f)
+    train_dataset = save['train_dataset']
+    train_labels = save['train_labels']
+    valid_dataset = save['valid_dataset']
+    valid_labels = save['valid_labels']
+    test_dataset = save['test_dataset']
+    test_labels = save['test_labels']
+    del save  # hint to help gc free up memory
+
+def statsStr(data, labels):
+    return "%d data points, %d labels" % (len(data), len(labels))
+
+print("done loading pickle file;\n\t[taining] %s\n\t[testing] %s\n\t[valids ] %s\n" % (
+    statsStr(train_dataset, train_labels),
+    statsStr(test_dataset, test_labels),
+    statsStr(valid_dataset, valid_labels)))
+
